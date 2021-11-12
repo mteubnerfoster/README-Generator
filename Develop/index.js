0d61729a2a40dcf773c28ae02c8fcc2fb0d0f26f
Megan Teubner-Foster <mteubnerfoster@gmail.com>
2021-11-11 18:45:31 -0800

    Updated Inquirer and added init function
    Added init function and performed an npm audit fix to update to Inquirer 8.2.0.

--- Develop/index.js
+++ Develop/index.js
@@ -63,7 +63,14 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((responses) => {
+        console.log('inquiry response: ', responses);
+        writeToFile('generatedREADME.md', generateMarkdown({...responses}));
+    })
+
+}
 
 // Function call to initialize app
 init();
